c809650bc8576391caeda127d1976f21898b1944
ChumakovaOlga <tresa1@mail.ru>
2021-08-17 10:15:03 +0300

    initiate project

---
+++ app123-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  app123-ios
+//
+//  Created by Olga Chumakova on 17.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
